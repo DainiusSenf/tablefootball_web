46433766486c176f1973659c741d26270b0cc699
dainius123 <lopas489756>
2016-05-12 23:16:25 +0300

    game elo change

--- js/controllerAddGame.js
+++ js/controllerAddGame.js
@@ -18,8 +18,7 @@ angular.module('addGame', ['ngMaterial'])
 	}
 
 	$scope.submit = function () {
-		$query = "js/php/addGame.php?player1=" + $scope.player1 + "&player2=" + $scope.player2 + "&player3=" + $scope.player3
-			 + "&player4=" + $scope.player4 + "&score1=" + $scope.score1 + "&score2=" + $scope.score2;
+		
 
 		if ($scope.player1 == undefined || $scope.player2 == undefined || $scope.player3 == undefined || $scope.player4 == undefined ||
 			$scope.score1 == '' || $scope.score2 == '' || $scope.score1 == undefined || $scope.score2 == undefined) {
@@ -30,18 +29,17 @@ angular.module('addGame', ['ngMaterial'])
 		if ($scope.player1 != $scope.player2 && $scope.player1 != $scope.player3 && $scope.player1 != $scope.player4
 			 && $scope.player2 != $scope.player3 && $scope.player2 != $scope.player4
 			 && $scope.player3 != $scope.player4) {
-			$http.post($query).success(function (data) {
-				console.log(data);
-				if ($scope.score1 == $scope.score2)
-					winnerTeam = 0;
-				else if ($scope.score1 > $scope.score2)
-					winnerTeam = 1;
-				else
-					winnerTeam = 2;
-				getPlayers();
-				
 
-			});
+			if ($scope.score1 == $scope.score2)
+				winnerTeam = 0;
+			else if ($scope.score1 > $scope.score2)
+				winnerTeam = 1;
+			else
+				winnerTeam = 2;
+			
+			getPlayers();
+			console.log(player1eloChange);
+			
 		} else {
 			alert("Same player can't be selected twice");
 		}
@@ -55,7 +53,7 @@ angular.module('addGame', ['ngMaterial'])
 	player1eloChange,
 	player2eloChange,
 	player3eloChange,
-	player4eloChange;;
+	player4eloChange;
 	
 	function updatePlayerStatistic(player, elo) {
 		
@@ -165,16 +163,24 @@ angular.module('addGame', ['ngMaterial'])
 				firstTeamWon(data);
 			else if (winnerTeam == 2)
 				secondTeamWon(data);
-
-			updatePlayerElo($scope.player1, player1newElo);
-			updatePlayerElo($scope.player2, player2newElo);
-			updatePlayerElo($scope.player3, player3newElo);
-			updatePlayerElo($scope.player4, player4newElo);
-			getUsers();
-			updatePlayerStatistic($scope.player1, player1newElo);
-			updatePlayerStatistic($scope.player2, player2newElo);
-			updatePlayerStatistic($scope.player3, player3newElo);
-			updatePlayerStatistic($scope.player4, player4newElo);
+			
+			$query = "js/php/addGame.php?player1=" + $scope.player1 + "&player2=" + $scope.player2 + "&player3=" + $scope.player3
+			 + "&player4=" + $scope.player4 + "&score1=" + $scope.score1 + "&score2=" + $scope.score2+ "&player1eloChange=" + player1eloChange+ 
+			 "&player2eloChange=" + player2eloChange+ "&player3eloChange=" + player3eloChange+ "&player4eloChange=" + player4eloChange;
+			$http.post($query).success(function (data) {
+				alert("Games saved");
+				console.log(data);
+				console.log(player1eloChange);
+				updatePlayerElo($scope.player1, player1newElo);
+				updatePlayerElo($scope.player2, player2newElo);
+				updatePlayerElo($scope.player3, player3newElo);
+				updatePlayerElo($scope.player4, player4newElo);
+				getUsers();
+				updatePlayerStatistic($scope.player1, player1newElo);
+				updatePlayerStatistic($scope.player2, player2newElo);
+				updatePlayerStatistic($scope.player3, player3newElo);
+				updatePlayerStatistic($scope.player4, player4newElo);
+			});
 		});
 	}
 
@@ -182,12 +188,16 @@ angular.module('addGame', ['ngMaterial'])
 		for (var i = 0; i < data.length; i++) {
 			if ($scope.player1 == data[i].username) {
 				player1newElo = getELOwin(data[i].elo, 30, data[2].elo, data[3].elo);
+				player1eloChange = player1newElo - parseInt(data[i].elo);
 			} else if ($scope.player2 == data[i].username) {
 				player2newElo = getELOwin(data[i].elo, 30, data[2].elo, data[3].elo);
+				player2eloChange = player2newElo - parseInt(data[i].elo);
 			} else if ($scope.player3 == data[i].username) {
 				player3newElo = getELOlose(data[i].elo, 30, data[0].elo, data[1].elo);
+				player3eloChange = player3newElo - parseInt(data[i].elo);
 			} else if ($scope.player4 == data[i].username) {
 				player4newElo = getELOlose(data[i].elo, 30, data[0].elo, data[1].elo);
+				player4eloChange = player4newElo - parseInt(data[i].elo);
 			}
 		}
 
@@ -197,12 +207,16 @@ angular.module('addGame', ['ngMaterial'])
 		for (var i = 0; i < data.length; i++) {
 			if ($scope.player1 == data[i].username) {
 				player1newElo = getELOlose(data[i].elo, 30, data[2].elo, data[3].elo);
+				player1eloChange = player1newElo - parseInt(data[i].elo);
 			} else if ($scope.player2 == data[i].username) {
 				player2newElo = getELOlose(data[i].elo, 30, data[2].elo, data[3].elo);
+				player2eloChange = player2newElo - parseInt(data[i].elo);
 			} else if ($scope.player3 == data[i].username) {
 				player3newElo = getELOwin(data[i].elo, 30, data[0].elo, data[1].elo);
+				player3eloChange = player3newElo - parseInt(data[i].elo);
 			} else if ($scope.player4 == data[i].username) {
 				player4newElo = getELOwin(data[i].elo, 30, data[0].elo, data[1].elo);
+				player4eloChange = player4newElo - parseInt(data[i].elo);
 			}
 		}
 
@@ -212,12 +226,16 @@ angular.module('addGame', ['ngMaterial'])
 		for (var i = 0; i < data.length; i++) {
 			if ($scope.player1 == data[i].username) {
 				player1newElo = getELOdraw(data[i].elo, 30, data[2].elo, data[3].elo);
+				player1eloChange = player1newElo - parseInt(data[i].elo);
 			} else if ($scope.player2 == data[i].username) {
 				player2newElo = getELOdraw(data[i].elo, 30, data[2].elo, data[3].elo);
+				player2eloChange = player2newElo - parseInt(data[i].elo);
 			} else if ($scope.player3 == data[i].username) {
 				player3newElo = getELOdraw(data[i].elo, 30, data[0].elo, data[1].elo);
+				player3eloChange = player3newElo - parseInt(data[i].elo);
 			} else if ($scope.player4 == data[i].username) {
 				player4newElo = getELOdraw(data[i].elo, 30, data[0].elo, data[1].elo);
+				player4eloChange = player4newElo - parseInt(data[i].elo);
 			}
 		}
 
--- js/php/addGame.php
+++ js/php/addGame.php
@@ -9,31 +9,32 @@
 			$player4 = $_GET['player4'];
 			$score1 = $_GET['score1'];
 			$score2 = $_GET['score2'];
+			$player1eloChange = $_GET['player1eloChange'];
+			$player2eloChange = $_GET['player2eloChange'];
+			$player3eloChange = $_GET['player3eloChange'];
+			$player4eloChange = $_GET['player4eloChange'];
 						
 			try{
 				if(isset($_GET['approved1'])){
-					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score)
-					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2')";
+					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score,team1user1score,team1user2score,team2user1score,team2user2score)
+					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2','$player1eloChange','$player2eloChange','$player3eloChange','$player4eloChange')";
 				} else if(isset($_GET['approved2'])){
-					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score)
-					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2')";
+					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score,team1user1score,team1user2score,team2user1score,team2user2score)
+					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2','$player1eloChange','$player2eloChange','$player3eloChange','$player4eloChange')";
 				} else if(isset($_GET['approved3'])){
-					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score)
-					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2')";
+					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score,team1user1score,team1user2score,team2user1score,team2user2score)
+					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2','$player1eloChange','$player2eloChange','$player3eloChange','$player4eloChange')";
 				} else if(isset($_GET['approved4'])){
-					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score)
-					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2')";
+					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score,team1user1score,team1user2score,team2user1score,team2user2score)
+					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2','$player1eloChange','$player2eloChange','$player3eloChange','$player4eloChange')";
 				} else {
-					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score)
-					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2')";
+					$sql = "INSERT INTO games(team1user1,team1user2,team2user1,team2user2, team1score,team2score,team1user1score,team1user2score,team2user1score,team2user2score)
+					VALUES ('$player1','$player2','$player3','$player4','$score1','$score2','$player1eloChange','$player2eloChange','$player3eloChange','$player4eloChange')";
 				}
 		
 				$stmt = $dbh->prepare( $sql );
 				$stmt->execute();
-				if( $stmt )
-					echo $json_response = json_encode(true);
-				else
-					echo $json_response = json_encode(false);
+					echo $json_response = json_encode($stmt);
 			} catch(PDOException $e){
 				echo $json_response = json_encode($e);
 			}
